26c922b5b1b746d2aac7ddb15e4c19b7238531b0
Anders Ohma <andersohma@gmail.com>
2023-05-09 12:44:00 +0200

    add plot accessors

--- fuvpy/src/accessor.py
+++ fuvpy/src/accessor.py
@@ -1,6 +1,7 @@
 import xarray as xr
 from fuvpy.src.background import backgroundmodel_BS,backgroundmodel_SH
 from fuvpy.src.boundaries import boundarymodel_BS,boundarymodel_F,detect_boundaries
+from fuvpy.src.plotting import plotimg,plot_backgroundmodel_BS,pplot,plot_ltlat,plotboundaries
 
 @xr.register_dataset_accessor("fuv")
 
@@ -9,6 +10,7 @@ class FuvAccessor:
     def __init__(self, xarray_obj):
         self._obj = xarray_obj
 
+    # Accessors to background models
     def backgroundmodel_BS(self,**kwargs):
         inplace = bool(kwargs.pop('inplace')) if 'inplace' in kwargs.keys() else False
         if inplace:
@@ -23,6 +25,7 @@ class FuvAccessor:
         else:
             return backgroundmodel_SH(self._obj,Nsh,Msh,**kwargs)
 
+    # Accessors to boundary models
     def detect_boundaries(self,**kwargs):
         return detect_boundaries(self._obj,**kwargs)
 
@@ -30,4 +33,21 @@ class FuvAccessor:
         return boundarymodel_BS(self._obj,**kwargs)
 
     def boundarymodel_F(self,**kwargs):
-        return boundarymodel_F(self._obj,**kwargs)   
+        return boundarymodel_F(self._obj,**kwargs)
+    
+    # Accessors to plot functions
+    def plotimg(self,inImg,**kwargs):
+        return plotimg(self._obj,inImg,**kwargs)
+
+    def plot_backgroundmodel_BS(self,**kwargs):
+        plot_backgroundmodel_BS(self._obj,**kwargs)
+
+    def pplot(self,inImg,**kwargs):
+        pplot(self._obj,inImg,**kwargs)
+
+    def plot_ltlat(self,inImg,**kwargs):
+        return plot_ltlat(self._obj,inImg,**kwargs)
+
+    def plotboundaries(self,boundary,**kwargs):
+        plotboundaries(self._obj,boundary,**kwargs)   
+    
