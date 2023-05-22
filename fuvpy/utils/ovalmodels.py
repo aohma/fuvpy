106aa24cc121296416ca8105903d39b8e11044bc
Anders Ohma <andersohma@gmail.com>
2023-05-22 09:53:54 +0200

    add more docstrings

--- fuvpy/utils/ovalmodels.py
+++ fuvpy/utils/ovalmodels.py
@@ -4,6 +4,28 @@ import pandas as pd
 
 
 def oval_starkov1994(mlt,al,boundary='pb'):
+    '''
+    Location of the auroral boundaries from the Starkov1994 model.
+
+    Starkov, G.V., Mathematical model of the auroral boundaries, Geomag. Aeron., 34 (3), 331–336, 1994a.
+    See: https://www.swsc-journal.org/articles/swsc/pdf/2011/01/swsc110021.pdf
+
+    Parameters
+    ----------
+    mlt : array_like
+        Local times to return boundary locations
+    al : array_like
+        Values of the AL index to use
+    boundary : str, optional
+        'pb' for poleward boundary and 'eb' for equatorward boundary. Default is 'pb'
+
+    Returns
+    -------
+    lat : array
+        Magnetic latitude of the boundaries with shape len(mlt) x len(al)
+
+    '''
+
     mlt = np.array([mlt]) if isinstance(mlt,(int,float)) else np.array(mlt)
     al = np.array([al]) if isinstance(al,(int,float)) else np.array(al) 
 
@@ -19,6 +41,37 @@ def oval_starkov1994(mlt,al,boundary='pb'):
     return 90-clat
 
 def oval_hu2017(Bx,By,Bz,Vp,Np,AE,boundary='pb'):
+    '''
+    Location of the auroral boundaries from the Hu2017 model
+
+    Hu, Z.-J., Yang, Q.-J., Liang, J.-M., Hu, H.-Q., Zhang, B.-C., and Yang, H.-G. (2017),
+    Variation and modeling of ultraviolet auroral oval boundaries associated with interplanetary and geomagnetic parameters,
+    Space Weather, 15, 606– 622, doi:10.1002/2016SW001530. 
+
+    Parameters
+    Bx : array_like
+        IMF Bx component (nT)
+    By : array_like
+        IMF By component (nT)
+    Bz : array_like
+        IMF Bz component (nT)
+    Vp : array_like
+        Solar wind speed (100 km/s)
+    Np : array_like
+        Solar wind proton density (cm^-3)
+    AE : array_like
+        AE index (nT)
+
+    boundary : str, optional
+        'pb' for poleward boundary and 'eb' for equatorward boundary. Default is 'pb'
+
+    Returns
+    -------
+    lat : array
+        Magnetic latitude of the boundaries from 0 to 23 magnetic local time [shape 24 x len(Bx)]
+
+    '''
+
     Bx = np.array([Bx]) if isinstance(Bx,(int,float)) else np.array(Bx)
     By = np.array([By]) if isinstance(By,(int,float)) else np.array(By)
     Bz = np.array([Bz]) if isinstance(Bz,(int,float)) else np.array(Bz)
@@ -92,7 +145,7 @@ def kp2al(kp):
     al : array_like
         converted values
     '''
-    
+
     c0 = 18
     c1 =-12.3
     c2 = 27.2
